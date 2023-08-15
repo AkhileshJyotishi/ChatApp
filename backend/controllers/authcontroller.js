7e683fbf9bbd229ed9a729de7045c23cb67ec1a2
AkhileshJyotishi <akhileshjyotishi1729@gmail.com>
2023-08-15 18:57:09 +0530

    checking photo system

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -5,48 +5,38 @@ const usermodel = require("../models/usermodel");
 const bcrypt = require("bcryptjs");
 const axios = require("axios");
 const { request } = require("express");
-const cloudinary = require('cloudinary').v2;
-require('dotenv').config();
-cloudinary.config({ 
-  cloud_name: process.env.CLOUD_NAME, 
-  api_key: process.env.API_KEY, 
-  api_secret: process.env.API_SECRET 
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
-
 
 const profileUploadfn = async (req, res) => {
   const userId = req.user._id.toString();
   try {
-      const result = await cloudinary.uploader.upload(req.file.path, {
-          public_id: `${userId}_profile`,
-          width: 500,
-          height: 500,
-          crop: 'fill'
-      })
-      const user = await usermodel.findOneAndUpdate(
-          { _id: userId },
-          { $set: { photourl: result.secure_url } },
-          { new: true, upsert: true }
-      )
-      if (user && result) {
-          res.status(200).json(result.secure_url);
-      }
+    const result = await cloudinary.uploader.upload(req.file.path, {
+      public_id: `${userId}_profile`,
+      width: 500,
+      height: 500,
+      crop: "fill",
+    });
+    const user = await usermodel.findOneAndUpdate(
+      { _id: userId },
+      { $set: { photourl: result.secure_url } },
+      { new: true, upsert: true }
+    );
+    if (user && result) {
+      res.status(200).json(result.secure_url);
+    }
+  } catch (err) {
+    res.status(400).json(err.message);
   }
-  catch (err) {
-      res.status(400).json(err.message);
-  }
-}
-
-
-
-
-
-
-
-
-
+};
 
 const register = async (req, res) => {
+  console.log(req.body);
   try {
     const { username, password, mail } = req.body;
     if (username && password && mail) {
@@ -64,35 +54,34 @@ const register = async (req, res) => {
         const hashpasswd = await bcrypt.hash(password, 10);
         var reply;
         // let photourl;
-        if(req.file.path){
-  // profileUploadfn(req,res);
-  const result = await cloudinary.uploader.upload(req.file.path, {
-    public_id: `doraemon`,
-    width: 500,
-    height: 500,
-    crop: 'fill'
-})
-  const user = new usermodel({
-    username,
-    password: hashpasswd,
-    mail,
-    photourl:result.secure_url
-  });
-   reply = await user.save();
-}
-else {
-  photourl=""
-  // console.log(reply)
-  console.log(hashpasswd)
+        if (req.file.path) {
+          // profileUploadfn(req,res);
+          const result = await cloudinary.uploader.upload(req.file.path, {
+            public_id: `doraemon`,
+            width: 500,
+            height: 500,
+            crop: "fill",
+          });
+          const user = new usermodel({
+            username,
+            password: hashpasswd,
+            mail,
+            photourl: result.secure_url,
+          });
+          reply = await user.save();
+        } else {
+          photourl = "";
+          // console.log(reply)
+          console.log(hashpasswd);
 
-  const user = new usermodel({
-    username,
-    password: hashpasswd,
-    mail,
-    photourl
-  });
-   reply = await user.save();
-}
+          const user = new usermodel({
+            username,
+            password: hashpasswd,
+            mail,
+            photourl,
+          });
+          reply = await user.save();
+        }
 
         // how to use graphql here
 
--- backend/routes/authroutes.js
+++ backend/routes/authroutes.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const Joi = require("joi");
-
+const formidable = require("express-formidable");
 const router = express.Router();
 // const app = express()
 const { login, register } = require("../controllers/authcontroller");
 
 const validator = require("express-joi-validation").createValidator({});
-
-const registeruserschema = Joi.object({
-  username: Joi.string().min(3).max(15).required(),
-  password: Joi.string().min(6).required(),
-  repeat_password: Joi.ref("password"),
-  mail: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
-    .required(),
-    photourl:Joi.string()
-});
+router.use(formidable());
+// const registeruserschema = Joi.object({
+//   username: Joi.string().min(3).max(15).required(),
+//   password: Joi.string().min(6).required(),
+//   repeat_password: Joi.ref("password"),
+//   mail: Joi.string()
+//     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+//     .required(),
+//   photourl: Joi.string(),
+// });
 const loginuserschema = Joi.object({
   username: Joi.string().min(3).max(15).required(),
   password: Joi.string().min(6).required(),
 });
 
-router.post("/register", validator.body(registeruserschema), register);
+router.post("/register", register);
+// validator.body(registeruserschema),
 router.post("/login", validator.body(loginuserschema), login);
 
 module.exports = router;
