6c07978cb2fe5a199d97360c629f737780d94dab
AkhileshJyotishi <akhileshjyotishi1729@gmail.com>
2023-08-17 00:04:49 +0530

    friends invitation need to be done

---
+++ backend/middlewares/authsocket.js
@@ -0,0 +1,22 @@
+const jwt=require('jsonwebtoken')
+require("dotenv").config();
+
+
+const verifytokensocket=(socket,next)=>{
+    const token= socket.handshake.auth?.token;
+
+    try{
+        const decoded=jwt.verify(token,process.env.JWTSecretKey)
+        socket.user=decoded
+
+    }
+    catch(err){
+        const socketerror=new Error("Not Authorized!")
+        return next(socketerror)
+    }
+    next();
+    
+}
+module.exports={
+    verifytokensocket
+}
