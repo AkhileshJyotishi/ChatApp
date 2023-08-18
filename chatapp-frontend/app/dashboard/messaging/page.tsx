0fcd8c0df5ae7e2feadb539ad3b1e1c1a4bada86
umeshSinghVerma <usv0308@gmail.com>
2023-08-18 16:20:36 +0530

    frontend for invitation accepted

--- chatapp-frontend/app/dashboard/messaging/page.tsx
+++ chatapp-frontend/app/dashboard/messaging/page.tsx
@@ -17,12 +17,28 @@ export default function Page() {
             imageUrl: ""
         }
     ]
+    const dara = [
+        {
+            "_id": "64df3edc8627375648426f59",
+            "senderId": {
+                "_id": "64dc9b7d5d0762a82138a381",
+                "username": "Akhilesh",
+                "mail": "abc@gmail.com",
+                "profile": "https://res.cloudinary.com/dfl98cqja/image/upload/v1692175041/doraemon.jpg"
+            },
+            "recieverId": "64df0c2a645169a09132c6f1",
+            "__v": 0
+        }
+    ]
 
 
     const { auth, setAuth } = useContext(AuthContext);
     const { friends2, setfriends, pendingfriendinvitations, setpendingfriendinvitations, onlineusers, setonlineusers, targetmailaddress, settargetmailaddress } = useContext(socketsContext);
     const [activeFriend, setActiveFriend] = useState<any>(null);
     const router = useRouter();
+
+
+
 
     useEffect(() => {
         if (!auth) {
@@ -68,6 +84,11 @@ export default function Page() {
         }
 
     }
+    function declineInvitation(recieverId,senderId){
+        
+    }
+    function acceptInvitation(recieverId,senderId){
+    }
 
 
 
@@ -92,9 +113,9 @@ export default function Page() {
             socket.on("friend", (data) => {
                 // console.log("socket m reieve ho rha h"); 
                 console.log(data);
-                // const {pendinginvitations}=data;
-                // setpendingfriendinvitations(pendinginvitations)
-              });
+                const { pendinginvitations } = data;
+                setpendingfriendinvitations(pendinginvitations)
+            });
         }
     }
 
@@ -141,85 +162,134 @@ export default function Page() {
                                     </span>
                                 </a>
                             </div>
-                            <div className="flex items-center">
-                                <div className="flex items-center ml-3 relative">
+                            <div className='flex gap-4 items-center'>
+                                <div className='relative'>
                                     <div>
-                                        <button
-                                            type="button"
-                                            className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                                            aria-expanded="false"
-                                            data-dropdown-toggle="dropdown-user"
-                                        >
-                                            <span className="sr-only">Open user menu</span>
-                                            <img
-                                                className="w-8 h-8 rounded-full"
-                                                src={`${auth.profile}`}
-                                                alt="user photo"
-                                                onClick={() => {
-                                                    document.getElementById("dropdown-user")?.classList.toggle("hidden");
-                                                }}
-                                            />
-                                        </button>
+                                        <img src="/notificationBell.svg" alt="" width={20} height={20}
+                                            onClick={() => {
+                                                document.getElementById("notification")?.classList.toggle("hidden");
+                                            }}
+                                        />
+                                    </div>
+                                    <div className='absolute -top-3 -right-2 text-xs text-white w-4 h-4 flex items-center justify-center bg-red-500 rounded-[50%]'>
+                                        {pendingfriendinvitations.length>0 && pendingfriendinvitations.length}
                                     </div>
                                     <div
-                                        className="z-50 hidden  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 absolute top-5 right-0"
-                                        id="dropdown-user"
+                                        className="z-50 hidden  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 absolute top-5 right-0 min-w-[200px]"
+                                        id="notification"
                                     >
-                                        <div className="px-4 py-3" role="none">
-                                            <p
-                                                className="text-sm text-gray-900 dark:text-white"
-                                                role="none"
+                                        {
+                                            pendingfriendinvitations.map((friend:any, key:number) => {
+                                                return (
+                                                    <>
+                                                        <div key={key} className='flex justify-between items-center w-full p-3'>
+                                                            <div className='flex gap-3 items-center'>
+                                                                <img
+                                                                    className="w-8 h-8 rounded-full border"
+                                                                    src={`${friend.senderId.profile}`}
+                                                                    alt="user photo"
+                                                                />
+                                                                <p>{friend.senderId.username}</p>
+                                                            </div>
+                                                            <div className='flex gap-2'>
+                                                                <button onClick={()=>{
+                                                                    declineInvitation(friend.recieverId,friend.senderId._id)
+                                                                }}>
+                                                                    <img src="/cross.svg" alt="" width={16} height={16} />
+                                                                </button>
+                                                                <button onClick={()=>{
+                                                                    acceptInvitation(friend.recieverId,friend.senderId._id)
+                                                                }}>
+                                                                    <img src="/check.svg" alt="" width={20} height={20}/>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                                <div className="flex items-center">
+                                    <div className="flex items-center ml-3 relative">
+                                        <div>
+                                            <button
+                                                type="button"
+                                                className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                                aria-expanded="false"
+                                                data-dropdown-toggle="dropdown-user"
                                             >
-                                                {auth.username}
-                                            </p>
-                                            <p
-                                                className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                                                role="none"
-                                            >
-                                                {auth.email || auth.mail}
-                                            </p>
-                                        </div>
-                                        <ul className="py-1" role="none">
-                                            <li>
-                                                <a
-                                                    href="#"
-                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    role="menuitem"
-                                                >
-                                                    Dashboard
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="#"
-                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    role="menuitem"
-                                                >
-                                                    Settings
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="#"
-                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    role="menuitem"
-                                                >
-                                                    Earnings
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <button
-                                                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                    role="menuitem"
+                                                <span className="sr-only">Open user menu</span>
+                                                <img
+                                                    className="w-8 h-8 rounded-full"
+                                                    src={`${auth.profile}`}
+                                                    alt="user photo"
                                                     onClick={() => {
-                                                        localStorage.removeItem('user');
-                                                        setAuth(null);
+                                                        document.getElementById("dropdown-user")?.classList.toggle("hidden");
                                                     }}
+                                                />
+                                            </button>
+                                        </div>
+                                        <div
+                                            className="z-50 hidden  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 absolute top-5 right-0"
+                                            id="dropdown-user"
+                                        >
+                                            <div className="px-4 py-3" role="none">
+                                                <p
+                                                    className="text-sm text-gray-900 dark:text-white"
+                                                    role="none"
                                                 >
-                                                    Sign out
-                                                </button>
-                                            </li>
-                                        </ul>
+                                                    {auth.username}
+                                                </p>
+                                                <p
+                                                    className="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+                                                    role="none"
+                                                >
+                                                    {auth.email || auth.mail}
+                                                </p>
+                                            </div>
+                                            <ul className="py-1" role="none">
+                                                <li>
+                                                    <a
+                                                        href="#"
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                        role="menuitem"
+                                                    >
+                                                        Dashboard
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="#"
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                        role="menuitem"
+                                                    >
+                                                        Settings
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <a
+                                                        href="#"
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                        role="menuitem"
+                                                    >
+                                                        Earnings
+                                                    </a>
+                                                </li>
+                                                <li>
+                                                    <button
+                                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                        role="menuitem"
+                                                        onClick={() => {
+                                                            localStorage.removeItem('user');
+                                                            setAuth(null);
+                                                        }}
+                                                    >
+                                                        Sign out
+                                                    </button>
+                                                </li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -296,24 +366,9 @@ export default function Page() {
                                         </div>
                                     </>
                                     : (
-                                        <>
-                                            <p>
-                                                Select some person to chat
-                                            </p>
-                                            <div>
-                                                {
-                                                    pendingfriendinvitations.map((invitation: any, key: any) => {
-                                                        return (
-                                                            <>
-                                                                <div>
-                                                                    invitation
-                                                                </div>
-                                                            </>
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                        </>
+                                        <div>
+                                            Select some friend to chat
+                                        </div>
                                     )
 
                             }
